fa2820efdba7bc88fb032343d3a8704e8e0fcddb
Ignorantbird <sarmisthadebnathsimi@gmail.com>
2025-09-05 18:43:03 +0530

    Resolve indexing issues

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -68,7 +68,7 @@ export const SEO = ({
       <meta name="twitter:image" content={`${siteUrl}${ogImage}`} />
       
       {/* Additional SEO */}
-      <meta name="robots" content="index, follow" />
+      <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       
       {/* Google Search Console Verification */}
